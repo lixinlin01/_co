23a4bfcc807637a0069176257b38b0bc8775f8af
sharkyyy <gnes37543043@gmail..com>
2025-12-09 11:31:40 +0800

    fill

--- homework/4/fill/Fill.asm
+++ homework/4/fill/Fill.asm
@@ -9,3 +9,78 @@
 // the screen should be cleared.
 
 //// Replace this comment with your code.
+
+// check keyboard
+(LOOP)
+@KBD      // KBD address
+D=M
+@FILL
+D;JNE       // if KBD != 0 jump to FILL
+@CLEAR
+0;JMP       // else go CLEAR
+
+// -------------------------------------------------
+// FILL: write -1 to screen (16384..24575 --> 8192 )
+
+(FILL)
+@SCREEN
+D=A
+@0
+M=D         // R0 = SCREEN start address (16384)
+
+
+@8192
+D=A
+@1
+M=D         // R1 = 8192 (counter)
+
+
+(FILL_LOOP)
+@0
+A=M
+M=-1        // addr = -1 (black line)
+
+@0
+M=M+1      // addr + 1
+
+@1
+M=M-1      // counter - 1
+D=M
+@FILL_LOOP
+D;JGT      // if counter > 0 --> FILL_LOOP
+
+@LOOP
+0;JMP
+
+// -------------------------------------------------
+// CLEAR: write 0 to screen (8192 words)
+(CLEAR)
+
+@SCREEN
+D=A
+@0
+M=D         // R0 = SCREEN start address (16384)
+
+
+@8192
+D=A
+@1
+M=D         // R1 = 8192 (counter)
+
+
+(CLEAR_LOOP)
+@0
+A=M
+M=0        // addr = 0 (white)
+
+@0
+M=M+1      // addr + 1
+
+@1
+M=M-1      // counter - 1
+D=M
+@CLEAR_LOOP
+D;JGT      // if counter > 0 --> CLEAR_LOOP
+
+@LOOP
+0;JMP
